3f6cd4c12096c5c1d1f514dd49f4082d125db52a
Anunay-Anand <anunayanand@gmail.com>
2021-09-13 02:35:11 +0530

    Added lazy loading for most files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 // importing react and react router dom
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 // Importing custom components
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 // The wrapper element for the entire page. Thus we can render Navbar side by side
 import Layout from "./components/layout/Layout";
 
 function App() {
+  // Using lazy loading and importing NewQuote only when it is required
+  const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+  const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+  const NotFound = React.lazy(() => import("./pages/NotFound"));
+  const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        {/* We Render all quotes and then specific quotes in different paths */}
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/new">
-          <NewQuote />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        {/* When nothing is found. Universal path 404 */}
-        <Route path="*">
-          <NotFound />
-        </Route>
+        <Suspense
+          fallback={
+            <article className="centered">
+              <LoadingSpinner />
+            </article>
+          }
+        >
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          {/* We Render all quotes and then specific quotes in different paths */}
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/new">
+            <NewQuote />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          {/* When nothing is found. Universal path 404 */}
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Suspense>
       </Switch>
     </Layout>
   );
