6280b25323d813a2f64f8328b1aa32bc772f24cd
Anunay-Anand <anunayanand@gmail.com>
2021-09-13 01:08:24 +0530

    added custom hook and api for firebase requests

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,19 +1,47 @@
-// Importing React and other libraries
-import React from "react";
+// Importing React and other libraries (hooks)
+import React, { useEffect } from "react";
+import useHttp from "../hooks/use-http";
 
 // Importing custom components
 import QuoteList from "../components/quotes/QuoteList";
-
-// Dummy data for quotes
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning so Fast" },
-  { id: "q2", author: "May", text: "Learning so Slow" },
-  { id: "q3", author: "Mayer", text: "Learning so Quick" },
-  { id: "q4", author: "Mayo", text: "Learning so Nicely" },
-];
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import { getAllQuotes } from "../lib/api";
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  // Using our custom hook to create a Request
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  // Using useEffect to automatically get all Quotes on loading
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  // If the status if pending return other component
+  if (status === "pending") {
+    return (
+      <article className="centered">
+        <LoadingSpinner />
+      </article>
+    );
+  }
+
+  // If there is an error
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  // If there is no data to show
+  if ((status === "completed" && !loadedQuotes) || loadedQuotes.length === 0) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,23 +1,36 @@
 // Importing React and other Important libraries
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 // Importing custom components
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  // Using custom hook to create Quote
+  const { sendRequest, status } = useHttp(addQuote);
   // Fetching the history object
   const history = useHistory();
 
+  // Using useEffect to check if the request is completed from custom hook
+  // It is based on status code
+  useEffect(() => {
+    if (status === "completed") {
+      // redirect the user on completion of creating quote
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   // Accepting the new quote Data coming from QuoteForm
   const addQuoteHandler = (data) => {
-    console.log(data);
-
-    // Sending user back to the quotes page
-    history.push("/quotes");
+    // Sending quote data to server using custom Hook
+    sendRequest(data);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
